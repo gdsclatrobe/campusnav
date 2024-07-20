c50485a9058a459c898e66357e261fb122b54081
Shyalan Ramesh <rshyalan@gmail.com>
2024-07-21 06:13:01 +1000

    navbar v1.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import Home from './pages/Home/Home';
 import './styles/App.css';
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+import logo from '../../img/nav_logo.png';
+
+const Navbar = () => {
+    return (
+      <div className="navbar-container">
+        <nav>
+          <a href="/" className="logo-link"><img src={logo} alt="S Logo" className="logo" /></a>
+          <ul className="nav-links">
+              <li>
+                <NavLink to="/" exact activeClassName="active">
+                  <i className="fas fa-home"></i> Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/about" activeClassName="active">
+                  <i className="fas fa-user"></i> About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/support" activeClassName="active">
+                  <i className="fas fa-life-ring"></i> Support
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/adminlogin" activeClassName="active">
+                  <i className="fas fa-sign-in-alt"></i> Login
+                </NavLink>
+              </li>
+          </ul>
+        </nav>
+      </div>
+    );
+};
+
+export default Navbar;
